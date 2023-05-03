dec0e9d138c02d5db3e043d309b9b2457c9b21aa
jesusoyer <oyervidesjesus017@gmail.com>
2023-05-03 15:40:51 -0500

    created buttons and install the prop-types package

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+function App(){
+    return(<div>hello
+
+
+        <div>
+            <Button success rounded outline>touch me</Button>
+        </div>
+        <div>
+            <Button>buy now</Button>
+        </div>
+        <div>
+            <Button>eat here</Button>
+        </div>
+        <div>
+            <Button>run fast</Button>
+        </div>
+
+
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ comps/src/Button.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+
+function Button({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded,
+})
+
+{
+    return <button>{children}</button>
+}
+
+
+Button.propTypes = {
+checkVariationValue: ({primary, secondary, success, warning, danger}) =>{
+    const count = Number(!!primary)
+    + Number(!!secondary)
+    + Number(!!warning)
+    + Number(!!success)
+    + Number(!!danger)
+
+if (count > 1){
+    return new Error('only one of primary, secondary, warning, success, danger can be true')
+}
+
+
+}
+
+};
+
+
+export default Button;

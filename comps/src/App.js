a5444baf0a4cbb6bc395e3ca05cb04d76cdddd4b
jesusoyer <oyervidesjesus017@gmail.com>
2023-05-03 17:16:53 -0500

    added ...rest to props and className

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,23 +1,31 @@
 import Button from "./Button";
-
+import {GoBell, GoBug,} from "react-icons/go"
 
 
 
 function App(){
+    const handleClick = () =>{
+        console.log('click!!')
+    };
+
+
+
+
+
     return(<div>hello
 
 
         <div>
-            <Button primary>touch me</Button>
+            <Button   primary onClick={handleClick} > <GoBell/> touch me</Button>
         </div>
         <div>
-            <Button secondary>buy now</Button>
+            <Button secondary rounded><GoBug/> buy now</Button>
         </div>
         <div>
-            <Button warning>eat here</Button>
+            <Button warning >eat here</Button>
         </div>
         <div>
-            <Button danger>run fast</Button>
+            <Button danger rounded>run fast</Button>
         </div>
 
 
